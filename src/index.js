86c9d3168200aa0f05576407888014eb1e465b52
Sergio Mendieta <sergiboka03@gmail.com>
2025-07-29 12:47:52 -0700

    Laptop to PC

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import {express} from "express"
+const app = express();
+const port = 8000
+
+app.get("/",(req,res) =>
+{
+    console.log("Get Request");
+    res.status(200).send("Success");
+})
+
+app.listen(port,() =>
+    {console.log(`Listening on port ${port}`);}
+);
