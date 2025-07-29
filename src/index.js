14befc4836e0a6ca4988912c04e4fa57f9696a55
Sergio Mendieta <mendiets@uci.edu>
2025-07-29 16:47:01 -0700

    Running fine with dev setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import {express} from "express"
+import express from "express";
 const app = express();
-const port = 8000
+const port = 8000;
 
 app.get("/",(req,res) =>
 {
     console.log("Get Request");
-    res.status(200).send("Success");
-})
+    res.status(200).send({"Success":"True"});
+});
 
 app.listen(port,() =>
     {console.log(`Listening on port ${port}`);}
