2b211b5b65ddd86e4e3096f5f9a0d07e4c9bf32d
Sergio Mendieta <mendiets@uci.edu>
2025-07-30 19:20:07 -0700

    Basic Google Sheets API functionality. Refer to api key and .env file for specifics

--- src/index.js
+++ src/index.js
@@ -1,13 +1,42 @@
 import express from "express";
+import dotenv from 'dotenv'; //Allows use of .env variables, w/ config
+import {google} from "googleapis";
+import { GoogleAuth } from "google-auth-library";
+
+dotenv.config();//Use with dotenv to use .env vars
+
+const auth = new GoogleAuth({ //Google Auth for Sheets API
+  keyFile: 'key.json', //API key is the file
+  scopes: ['https://www.googleapis.com/auth/spreadsheets'], //The resource we will use
+});
+
 const app = express();
 const port = 8000;
 
-app.get("/",(req,res) =>
+app.get("/", async (req,res) =>
 {
     console.log("Get Request");
+  const sheets = google.sheets({ version: 'v4', auth: auth }); //"Instantiate" Sheet Resource
+
+  const spreadsheetId = process.env.sheetid; //SpreadSheet ID
+  const range = 'Sheet1!A1:B2';//Range of values
+  
+  try{
+    const response = await sheets.spreadsheets.values.get({ //Get request using sheet resource to retrieve the values specified by range from the spreadsheet 
+        spreadsheetId,
+        range,
+    });
+    console.log(`Success in getting info: ${response.data.values}`);
+  }
+  catch(err)
+  {
+    console.log("Error in Getting Info" + err);
+  }
+
     res.status(200).send({"Success":"True"});
 });
 
 app.listen(port,() =>
     {console.log(`Listening on port ${port}`);}
 );
+
